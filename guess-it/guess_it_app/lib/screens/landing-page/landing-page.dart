c717743b55caf82a5f0fc8de0854f020355334ee
PJscp16 <50209613+PJscp16@users.noreply.github.com>
2020-11-02 19:19:36 +0000

    Done Landing Page
    Still need to change "Enter as Admin" button to the end of the screen

--- guess-it/guess_it_app/lib/screens/landing-page/landing-page.dart
+++ guess-it/guess_it_app/lib/screens/landing-page/landing-page.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class LandingPanel extends StatefulWidget {
+class LandingPanel extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:
+      Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Image(
+                image: AssetImage('images/logo.jpg'),
+              ),
+            ),
+            SizedBox(height: 40),
+            Container(
+              child: Text(
+                'GuessIt',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            Container(
+              child: RaisedButton(
+                onPressed: () => {},
+                color: Colors.white,
+                textColor: Colors.black54,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)
+                ),
+                padding: EdgeInsets.fromLTRB(80, 0, 80, 0),
+                elevation: 5,
+                colorBrightness: Brightness.dark,
+                child: Text(
+                    'PLAY',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                ),
+              ),
+            ),
+            Container(
+              child: FlatButton(
+                child: Text(
+                    'Enter as Admin',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                      decoration: TextDecoration.underline
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Color.fromRGBO(134, 232, 214, 1.0),
+    );
   }
-
 }
