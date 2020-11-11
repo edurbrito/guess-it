6214e62691b30f02b4e3e8ecffc5f702b26c4946
Paulo Ribeiro <pjsalgadomribeiro@gmail.com>
2020-11-11 15:57:21 +0000

    "Enter as Admin" button at the end of screen

--- guess-it/guess_it_app/lib/screens/landing-page/landing-page.dart
+++ guess-it/guess_it_app/lib/screens/landing-page/landing-page.dart
@@ -8,70 +8,84 @@ class LandingPanel extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body:
-      Align(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+        Column(
+        children: <Widget>[
+            SizedBox(height: 170),
             Container(
-              child: Image(
-                image: AssetImage('images/logo.jpg'),
-              ),
-            ),
-            SizedBox(height: 40),
-            Container(
-              child: Text(
-                'GuessIt',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            SizedBox(height: 40),
-            Container(
-              child: RaisedButton(
-                onPressed: () => {},
-                color: Colors.white,
-                textColor: Colors.black54,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0)
-                ),
-                padding: EdgeInsets.fromLTRB(80, 0, 80, 0),
-                elevation: 5,
-                colorBrightness: Brightness.dark,
-                child: Text(
-                    'PLAY',
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Image(
+                      image: AssetImage('images/logo.jpg'),
                     ),
-                ),
+                  ),
+                  SizedBox(height: 40),
+                  Container(
+                    child: Text(
+                      'GuessIt',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  Container(
+                    child: RaisedButton(
+                      onPressed: () => {},
+                      color: Colors.white,
+                      textColor: Colors.black54,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)
+                      ),
+                      padding: EdgeInsets.fromLTRB(80, 0, 80, 0),
+                      elevation: 5,
+                      colorBrightness: Brightness.dark,
+                      child: Text(
+                        'PLAY',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Container(
-              child: FlatButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => AdminCode()),
-                  );
-                },
-                child: Text(
-                    'Enter as Admin',
-                  style: TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.white,
-                      decoration: TextDecoration.underline
-                  ),
+            Expanded(
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => AdminCode()),
+                        );
+                      },
+                      child: Text(
+                        'Enter as Admin',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.white,
+                          decoration: TextDecoration.underline
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
           ],
         ),
-      ),
       backgroundColor: Color.fromRGBO(134, 232, 214, 1.0),
-    );
+      );
   }
 }
