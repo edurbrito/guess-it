7e84162244974a93e12705fc3112bcee0c4545bd
Pedro <up201809694@fe.up.pt>
2020-12-11 12:51:00 +0000

    First unit test done

--- guess-it/guess_it_app/test/app.dart
+++ guess-it/guess_it_app/test/app.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:guess_it_app/main.dart';
+import 'package:guess_it_app/screens/admin-code-page/admin-code-page.dart';
+import 'package:guess_it_app/screens/landing-page/landing-page.dart';
+import 'package:mockito/mockito.dart';
 
-void main() async {
-  // This line enables the extension
-  enableFlutterDriverExtension();
+class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
-  // Call the `main()` function of your app or call `runApp` with any widget you
-  // are interested in testing.
-  runApp(MyApp());
+void main() {
+  testWidgets('Button is present and triggers navigation after tapped',
+          (WidgetTester tester) async {
+        final mockObserver = MockNavigatorObserver();
+        await tester.pumpWidget(
+          MaterialApp(
+            home: LandingPanel(),
+            navigatorObservers: [mockObserver],
+          ),
+        );
+
+        expect(find.byType(FlatButton), findsOneWidget);
+        await tester.tap(find.byType(FlatButton));
+        await tester.pumpAndSettle();
+
+        /// Verify that a push event happened
+        verify(mockObserver.didPush(any, any));
+
+        /// You'd also want to be sure that your page is now
+        /// present in the screen.
+        expect(find.byType(AdminCode), findsOneWidget);
+      });
 }
